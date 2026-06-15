41e40132e340b51bea343d8aa49a03e854d51239
JoseCunhaTeixeira <jose.cunha-teixeira@protonmail.com>
2026-06-15 20:02:28 +0200

    wip

--- src/masw/io/paths.py
+++ src/masw/io/paths.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 
-INPUT_DIR = PROJECT_ROOT / "input"
+INPUT_DIR = PROJECT_ROOT / "data/input"
+
+OUTPUT_DIR = PROJECT_ROOT / "data/output"
